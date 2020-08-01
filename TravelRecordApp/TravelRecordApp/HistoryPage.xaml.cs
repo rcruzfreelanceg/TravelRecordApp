509be657793f0b7edf55580d9ffe9e82e1724137
Ricky <rcruz78@outlook.com>
2020-08-01 15:45:55 -0400

    Initial Commit

---
+++ TravelRecordApp/TravelRecordApp/HistoryPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HistoryPage : ContentPage
+    {
+        public HistoryPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+    }
+}
---
+++ TravelRecordApp/TravelRecordApp/NewTravelPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelRecordApp.Model;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewTravelPage : ContentPage
+    {
+        public NewTravelPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Post post = new Post()
+            { 
+                Experience = experienceEntry.Text
+            };
+
+            SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation);
+            conn.CreateTable<Post>();
+            var rows = conn.Insert(post);
+
+            if (rows > 0)
+            {
+                DisplayAlert("Success", $"Experience succesfully inserted","Ok");
+            }
+            else
+            {
+                DisplayAlert("Failure", $"Experience failedto be inserted", "Ok");
+
+            }
+            conn.Close();
+        }
+    }
+}
